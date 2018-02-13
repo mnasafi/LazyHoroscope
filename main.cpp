c29a8e48cd5248cb4a49b82d358b70891f7251b4
mnasafi <mnasafi@mail.csuchico.edu>
2018-02-12 17:44:33 -0800

    declared variables and added prompt for name

--- main.cpp
+++ main.cpp
@@ -1,16 +1,57 @@
-//Authors:
+//Authors: Mustafa Nasafi
+
 #include<iostream>
-#include<string>
+#include<string> //string class library
+
 
 using namespace std;
 
 int main()
 {
-  //declare variables
 
-  //get user input
+    //declare variables
+    
+    string firstName;
+    string lastName;
+ 
+    int repeatAmount;
 
-  //tell fortune
+    //get user input
 
+    cout << "Hello!\n";
+    cout << "What's your first name?\n";
+
+    cin >> firstName;
+
+    cout << "What's your last name?\n";
+    
+    cin >> lastName;
+
+
+/*
+    // test and use varialbes
+
+    cout << "How many times should I repeat your name?\n";
+    cin >> repeatAmount;
+
+
+    // for loop to repeat name x times
+
+    for(int x = 0;  x < repeatAmount; x++ )
+    {
+        cout << "Your full name is: " << firstName << " " << lastName << endl;
+
+    } // end for loop
+
+*/
+
+
+
+
+
+    //tell fortune
+
+
+  //end program  
   return 0;
-}
+} 
