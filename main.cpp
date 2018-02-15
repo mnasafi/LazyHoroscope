0a1228f179208c3fda323ba2c5ab3d43b2fd5253
mnasafi <mnasafi@mail.csuchico.edu>
2018-02-14 18:27:36 -0800

    latest revision, to be continued

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main()
     string nickName ="";
     int lucky;
     
+    string f; // firstName 
+    string l; // lastName for if statement
 
     int repeatAmount;
 
@@ -46,6 +48,36 @@ int main()
     lucky = firstName.length();
     cout << "Your lucky number is: " << lucky << endl;  // display lucky number
     
+
+    //part 2
+
+    //initialize variables
+    f = firstName;
+    l = lastName;
+
+    if(f[0]=='a' || f[0]=='A' || f[0]=='e' || f[0]=='E' || f[0]=='i' || f[0]=='I' || f[0]=='o' || f[0]=='O' || f[0]=='u' || f[0]=='U' )
+    {
+        cout << "You should keep a low profile.\n";
+
+    }
+
+    else
+    {
+
+        cout << "You are destined to be famous!\n";
+
+    } //end if else
+
+
+
+    // last letter retriever
+    /*
+    firstName[len -1]
+    */
+
+
+
+    cout << "Have a good day!\n";
 
 
   //end program  
